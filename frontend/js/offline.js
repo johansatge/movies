aede7a59a9f71b4b1483e2c3c16dc0abe9ad5787
Johan Satgé <johansatge@users.noreply.github.com>
2018-03-16 00:28:59 +0100

    Offline UI base with interactions

--- frontend/js/offline.js
+++ frontend/js/offline.js
@@ -1,16 +1,38 @@
 /* global document, navigator */
 
 const nodeBody = document.body
-const nodeButton = document.querySelector('[data-js-save-offline]')
+const nodeOverlay = document.querySelector('[data-js-overlay-offline]')
+const nodeSaveButton = document.querySelector('[data-js-save-offline]')
+const nodeCancelButton = document.querySelector('[data-js-cancel-offline]')
+const nodeProgressBar = document.querySelector('[data-js-progress-offline]')
 
 export function init() {
-  if (!navigator || !navigator.serviceWorker) {
+  if (!isServiceWorkerAvailable()) {
     return
   }
   nodeBody.dataset.jsWithSw = true
-  nodeButton.addEventListener('click', onSaveOffline)
+  nodeSaveButton.addEventListener('click', onSaveOffline)
+  nodeCancelButton.addEventListener('click', onCancelOffline)
+}
+
+function isServiceWorkerAvailable() {
+  if (!navigator || !navigator.serviceWorker) {
+    return false
+  }
+  if (document.location.protocol === 'http:' && document.location.hostname !== 'localhost') {
+    return false
+  }
+  return true
 }
 
 function onSaveOffline() {
-  console.log('@todo save offline')
+  nodeOverlay.style.display = 'block'
+
+  console.log('@todo save offline', nodeProgressBar)
+}
+
+function onCancelOffline() {
+  nodeOverlay.style.display = 'none'
+
+  console.log('@todo cancel offline')
 }
