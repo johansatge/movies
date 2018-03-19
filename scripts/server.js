d949e9c62622b6b347e552930699013dd793575d
Johan Satgé <johansatge@users.noreply.github.com>
2018-03-19 21:21:33 +0100

    Local "no-cache" service worker response

--- scripts/server.js
+++ scripts/server.js
@@ -7,6 +7,10 @@ const path = require('path')
 const app = express()
 
 app.use(compression())
+app.get('/serviceworker.js', (request, response) => {
+  response.set('Cache-Control', 'no-cache, no-store, must-revalidate, max-age=0')
+  response.status(200).sendFile(path.join(__dirname, '..', '.dist', 'serviceworker.js'))
+})
 app.use('/', express.static(path.join(__dirname, '..', '.dist')))
 
 app.listen(5000, function() {
