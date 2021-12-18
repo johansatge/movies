89dac9c067f2a44136cae86269f60c46a5c3ea37
Johan Satgé <johansatge@users.noreply.github.com>
2021-12-18 13:55:20 +0100

    Fix import-refresh task

--- scripts/import-refresh.js
+++ scripts/import-refresh.js
@@ -5,18 +5,22 @@ const { log } = require('./helpers/log.js')
 const { emptyDir } = require('fs-extra')
 
 ;(async () => {
-  await emptyDir(path.join(__dirname, '../movies/posters'))
+  const postersPath = path.join(__dirname, '../movies/posters')
   const moviesPath = path.join(__dirname, '../movies')
+  try {
+    await fsp.rm(postersPath, { recursive: true })
+  } catch() {}
+  fsp.mkdir(postersPath, { recursive: true})
   let files = await fsp.readdir(moviesPath)
   files = files.filter((file) => file.endsWith('.json'))
   for(let fileIndex = 0; fileIndex < files.length; fileIndex += 1) {
-    const rawJson = await fsp.readFile(files[fileIndex], 'utf8')
+    const rawJson = await fsp.readFile(path.join(moviesPath, files[fileIndex]), 'utf8')
     const json = JSON.parse(rawJson)
     for(let movieIndex = 0; movieIndex < json.length; movieIndex += 1) {
       log(`Updating movie ${movieIndex} over ${json.length} (${files[fileIndex]}`)
       const oldData = json[movieIndex]
       json[movieIndex] = await fetchFormattedMovieData(json[movieIndex].tmdb_id, oldData.rating, oldData.watch_date)
     }
-    await fsp.writeFile(files[fileIndex], JSON.stringify(json, null, 2))
+    await fsp.writeFile(path.join(moviesPath, files[fileIndex]), JSON.stringify(json, null, 2))
   }
 })()
